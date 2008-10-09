ff53e6a5927e90a636eb8c27dae61e83bbb6fb7e
Shawn Sustrich <ssustrich@telesto.(none)>
2008-10-09 13:07:38 -0400

    first two files

---
+++ src/Dice.java
@@ -0,0 +1,22 @@
+import java.util.Vector;
+
+public class Dice {
+
+	public static int roll(int Rolls, int diceType){
+		int value= 0;
+		for (int x=0; x<Rolls; x++){
+			RandomNumberGenerator.randomInt(1, diceType);
+			value = value + RandomNumberGenerator.randomInt(1, diceType);
+		}
+		return value;
+	}
+	
+	public static int roll(Vector Rolls){
+//		Also, maybe another method that takes a vector of Roll objects and
+//		returns the total of the entire set AND populates the results
+//		individually.  That call would look like:
+		
+		
+		return 0;
+	}
+}
---
+++ src/RandomNumberGenerator.java
@@ -0,0 +1,30 @@
+import java.math.*;
+import java.util.Random;
+
+
+public class RandomNumberGenerator {
+
+	public int randomInt(int Max){
+		Random iRandom = new Random();
+		int randint = iRandom.nextInt(Max);
+		return randint;
+	}
+	
+	public static int randomInt(int Min, int Max){
+		Random iRandom = new Random();
+		int randint = iRandom.nextInt(Max)+Min;
+		return randint;
+	}
+	
+	public int randomInt(){
+		Random iRandom = new Random();
+		int randint = iRandom.nextInt();
+		return randint;
+	}
+	
+	public boolean randomBoolean(){
+		Random bRandom = new Random();
+		return bRandom.nextBoolean();
+	}
+	
+}
